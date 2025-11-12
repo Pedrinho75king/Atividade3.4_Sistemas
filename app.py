c2b2709e27cc17c4a764a164de5bc65f7388aec9
Pedrinho75king <pedroyan@colegiopaulodetarso.com.br>
2025-11-12 14:50:46 +0000

    2

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "<h1>Hello, Flask !!</h1>", 200
+
+@app.route("/versao")
+def versao():
+    versao = "1.1.0"
+    return f"App v{versao}", 200
+
